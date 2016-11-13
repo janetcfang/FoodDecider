d3b1e028542e345fabf5f6c82aaad15badae00c5
Felix Tiet <ftiet21@gmail.com>
2016-11-13 00:20:03 -0600

    cleanup

--- DecisionMaker.py
+++ DecisionMaker.py
@@ -3,44 +3,51 @@ import json
 import random
 from datetime import datetime as dt
 
-gmaps = gm.Client(key = "")
+gmaps = gm.Client(key = "AIzaSyBByCmegtekUSBBXj-OG5AAuhc98GDDfhs")
 
-#user input
-#userLoc = raw_input("Please enter your current location: " )
+#GET LOCATION
+
 #userAddress = input("Please enter your current address: ")
-#userAddress = "36.1447034,-86.8048438"
-
 userAddress = "2201 West End Ave, Nashville, TN 37235"
 geoc = gmaps.geocode(address=userAddress)
 
+#latitude and longitude
 loc = str(geoc[0]['geometry']['location']['lat']) + "," + str(geoc[0]['geometry']['location']['lng'])
 #print(loc)
 
 userChoice = input("What kind of food are you craving? (leave blank if undecided): ")
 search = userChoice + "food"
-restaurants = gmaps.places(search, location=loc, radius=8050, language="english")
 
+#driving distance
+userDistance = float(input("How far are you willing to drive? (miles): "))
+distance = userDistance * 1609.34   #converts meters to miles
+
+restaurants = gmaps.places(search, location=loc, radius=userDistance, language="english")
 #print(json.dumps(restaurants, sort_keys=True, indent=2))
 
-randQ = raw_input("\nWould you like us to randomly generate a restaurant for you? Yes or No: ")
-if randQ == "Yes":
-	new = "Yes"
-	while(new != "No"):
-		rand = random.randrange(len(restaurants['results']))
-		print(restaurants['results'][rand]['name'])
-		new = raw_input("\nWould you like a different restaurant? Yes or No: ")
-	choice = rand
-else:
-	choice = int(raw_input("\nWhich restaurant would you like directions to? "))
+for i in range(len(restaurants['results'])):
+    print('%d: %s - Rating: %s' % (i, restaurants['results'][i]['name'], restaurants['results'][i]['rating']))
 
+#random generation
+randQ = input("\nWould you like us to randomly generate a restaurant for you? Yes or No: ")
+if randQ == "Yes":
+    new = "Yes"
+    while new != "No":
+        rand = random.randrange(len(restaurants['results']))
+        print(restaurants['results'][rand]['name'])
+        new = input("\nWould you like a different restaurant? Yes or No: ")
+    choice = rand
+else:
+    choice = int(input("\nWhich restaurant would you like directions to? "))
 
 now = dt.now()
 
-directions = gmaps.directions(userAddress, restaurants['results'][choice]['formatted_address'], mode='driving',
+directions = gmaps.directions(loc, restaurants['results'][choice]['formatted_address'], mode='driving',
                               departure_time=now)
 
 #print(json.dumps(directions, indent=2))
 
+#directions to food
 for i in range(len(directions[0]['legs'][0]['steps'])):
     fix_string = directions[0]['legs'][0]['steps'][i]['html_instructions'].replace("<b>", "")
     fix_string = fix_string.replace("</b>", "")
@@ -50,4 +57,3 @@ for i in range(len(directions[0]['legs'][0]['steps'])):
         fix_string = fix_string[:format]
 
     print('Step %d: %s ' % (i+1, fix_string))
-
