46eb26c6656b1703a05349b538c43e70afa78af2
ftiet <ftiet21@gmail.com>
2016-11-12 21:56:34 -0600

    Fixes

--- DecisionMaker.py
+++ DecisionMaker.py
@@ -2,23 +2,21 @@ import googlemaps as gm
 import json
 from datetime import datetime as dt
 
-gmaps = gm.Client(key = "AIzaSyBByCmegtekUSBBXj-OG5AAuhc98GDDfhs")
+gmaps = gm.Client(key = "")
 
-#loc = "36.1432074,-86.8060411"
 
 #user input
 #userLoc = raw_input("Please enter your current location: " )
 userAddress = raw_input("Please enter your current address: ")
-<<<<<<< HEAD
-=======
+
 loc = userAddress
->>>>>>> f79a84676dcf2a5b46a87c6f492c8c7094689b3a
+
 userChoice = raw_input("What kind of food are you deciding on? ")
 
 restaurants = gmaps.places("restaurant", location=userAddress, radius=8050, language="english")
 
 #print(json.dumps(restaurants, sort_keys=True, indent=2))
-<<<<<<< HEAD
+
 for i in range(10):
     print('%d: %s - %s' % (i, restaurants['results'][i]['name'], restaurants['results'][i]['formatted_address']))
 
@@ -34,14 +32,8 @@ for direct in jobj['legs']['steps']:
     print(direct['html_instructions'])
 
 print(json.dumps(directions, indent=2))
-=======
+
 
 #list results 
 for i in range(10):
     print(i + '%s - %s' % (restaurants['results'][i]['name'], restaurants['results'][i]['formatted_address']))
-
-#user pick results
-
-
-
->>>>>>> f79a84676dcf2a5b46a87c6f492c8c7094689b3a
