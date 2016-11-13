f79a84676dcf2a5b46a87c6f492c8c7094689b3a
Janet Fang <janetcfang@gmail.com>
2016-11-12 21:38:25 -0600

    Update DecisionMaker.py

--- DecisionMaker.py
+++ DecisionMaker.py
@@ -4,12 +4,23 @@ from datetime import datetime as dt
 
 gmaps = gm.Client(key = "")
 
+#loc = "36.1432074,-86.8060411"
 
-loc = "36.1432074,-86.8060411"
+#user input
+#userLoc = raw_input("Please enter your current location: " )
+userAddress = raw_input("Please enter your current address: ")
+loc = userAddress
+userChoice = raw_input("What kind of food are you deciding on? ")
 
 restaurants = gmaps.places("restaurant", location=loc, radius=8050, language="english")
 
 #print(json.dumps(restaurants, sort_keys=True, indent=2))
 
+#list results 
 for i in range(10):
     print(i + '%s - %s' % (restaurants['results'][i]['name'], restaurants['results'][i]['formatted_address']))
+
+#user pick results
+
+
+
