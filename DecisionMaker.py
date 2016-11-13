d1fa04db0ab8d562bcbfeba0d4b95e9ef79c4a59
ftiet <ftiet21@gmail.com>
2016-11-12 22:46:11 -0600

    Directions!

--- DecisionMaker.py
+++ DecisionMaker.py
@@ -2,22 +2,19 @@ import googlemaps as gm
 import json
 from datetime import datetime as dt
 
-gmaps = gm.Client(key = "")
-
+gmaps = gm.Client(key = "AIzaSyBByCmegtekUSBBXj-OG5AAuhc98GDDfhs")
 
 #user input
 #userLoc = raw_input("Please enter your current location: " )
-userAddress = raw_input("Please enter your current address: ")
+#userAddress = input("Please enter your current address: ")
 
-loc = userAddress
-
-userChoice = raw_input("What kind of food are you deciding on? ")
-
+#userChoice = input("What kind of food are you deciding on? ")
+userAddress = "36.1447034,-86.8048438"
 restaurants = gmaps.places("restaurant", location=userAddress, radius=8050, language="english")
 
 #print(json.dumps(restaurants, sort_keys=True, indent=2))
 
-for i in range(10):
+for i in range(len(restaurants['results'])):
     print('%d: %s - %s' % (i, restaurants['results'][i]['name'], restaurants['results'][i]['formatted_address']))
 
 choice = int(input("Which restaurant would you like directions to? "))
@@ -27,13 +24,8 @@ now = dt.now()
 directions = gmaps.directions(userAddress, restaurants['results'][choice]['formatted_address'], mode='driving',
                               departure_time=now)
 
-jobj = json.load(directions)
-for direct in jobj['legs']['steps']:
-    print(direct['html_instructions'])
+#print(json.dumps(directions, indent=2))
 
-print(json.dumps(directions, indent=2))
+for i in range(len(directions[0]['legs'][0]['steps'])):
+    print('%d: %s ' % (i+1, directions[0]['legs'][0]['steps'][i]['html_instructions']))
 
-
-#list results 
-for i in range(10):
-    print(i + '%s - %s' % (restaurants['results'][i]['name'], restaurants['results'][i]['formatted_address']))
